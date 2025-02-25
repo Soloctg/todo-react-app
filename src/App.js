5fd9e851bac184b413c25f2b87b109ae3c0fdc4e
solomon-ctg <solomon.iroegbu@gmail.com>
2025-02-25 10:57:21 +0100

    feat: Add Note

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 
 import Header from "./components/Header";
-//import { Header }from "./components";
+import CreateArea from "./components/CreateArea";
+import React, { useState } from "react";
+
 
 function App() {
+	const [videos, setVideos] = useState([]);
+	const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+
+
   return (
     <div className="App">
 
 	    <Header />
+	    <CreateArea />
 
     </div>
   );
